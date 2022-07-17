ff76440680292ceb7375d884fc8d80ab05bd0d0d
Joel S Joy <52008820+joelsjoyt@users.noreply.github.com>
2022-07-17 11:06:19 +0530

    Minor fix
    Removed scrolling when modal is on

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,8 @@ const Home = ({
   if (loading) return null
   
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh] 
+    ${showMmodal && '!h-screen overflow-hidden'}`}>
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
